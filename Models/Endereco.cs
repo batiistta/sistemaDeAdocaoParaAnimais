24c9f0583bea1ec1d809b3c0e998e8762c33dfe1
Pauloocm <paulooo.cm@gmail.com>
2022-09-06 17:16:08 -0300

    fet: duas novas classes na pasta Models

---
+++ Models/Endereco.cs
@@ -0,0 +1,11 @@
+namespace sistemaDeAdocaoParaAnimais.Models
+{
+    public class Endereco
+    {
+        private string Estado { get; set; }
+        private string Municipio { get; set; }
+        private string Bairro { get; set;}
+        public string Cep { get; set; }
+
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,10 @@
+namespace sistemaDeAdocaoParaAnimais.Models
+{
+    public class Pessoa
+    {
+        private string Nome { get; set; }
+        private string Cpf { get; set;}
+        private string Sexo { get; set;}
+
+    }
+}
