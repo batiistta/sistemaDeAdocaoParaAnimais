b1ede647e752ca321011421099b665cbb68ba995
Rafael Rascado <99145004+Rafaelocm@users.noreply.github.com>
2022-09-17 20:29:25 +0000

    Testando api de correio

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,3 +5,20 @@ faqs.forEach(faq => {
         faq.classList.toggle("active");
     });
 });
+
+jQuery(function($){
+    $("input[name=cep]".change(function (){
+        var cep_code = $(this).val();
+        if(cep_code.length <= 0) return; 
+        $.get("http://apps.widenet.com.br/busca-cep/api/json", { code: cep_code }, function (result) {
+        if(result.status != 1 ){
+            alert(result.message || "Houve um erro desconhecido");
+            return; 
+        }
+        $("input[name='cep'").val(result.code);
+        $("input[name='estado'").val(result.state);
+        $("input[name='cidade'").val(result.city);
+        $("input[name='bairro'").val(result.district);    
+    });
+    }));
+});
