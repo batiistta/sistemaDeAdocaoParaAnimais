cad08697aa343c64ecd417b3f5445dae1d1f169e
Paulo César <paulocry67@gmail.com>
2022-09-17 16:48:37 -0300

    Feat / GroupPush

--- Models/Endereco.cs
+++ Models/Endereco.cs
@@ -2,8 +2,11 @@ namespace sistemaDeAdocaoParaAnimais.Models
 {
     public class Endereco
     {
+        private string Cep { get; set; }
         private string Estado { get; set; }
         private string Cidade { get; set; }
+        private string Bairro { get: set; }
+        private string Rua { get; set;}
 
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,11 +2,12 @@ namespace sistemaDeAdocaoParaAnimais.Models
 {
     public class Usuario
     {
+        private int Id { get; set;}
         private string Nome { get; set; }
         private string Email { get; set;}
         private string Senha { get; set;}
         private string Telefone { get; set; }
-         private Endereco Endereco { get; set; }
+        private Endereco Endereco { get; set; }
         
     }
 }
