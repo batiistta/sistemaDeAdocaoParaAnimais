84b8cd205ec37b222f36b4f81fde7ce10035b016
Rafaelocm <rafaelrascado98@gmail.com>
2022-09-20 09:33:00 -0300

    Feat: adicionando a classe caracteristica

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -10,17 +10,7 @@ namespace sistemaDeAdocaoParaAnimais.Models.Animal
         private int Porte { get; set; }
         private bool Filhote { get; set; }
         private bool Adulto { get; set; }
-        private int Energia { get; set; }
-        private int Humor { get; set; }
-        private int Apego { get; set; }
-        private int Latido { get; set; }
-        private int Inteligencia { get; set; }
-        private int Brincadeira { get; set; }
-        private int Adestramento { get; set; }
-        private bool Vacinado { get; set; }
-        private bool Castrado { get; set; }
-        private bool Vermifugado { get; set; }
-
+        private Caracteristica caracteristica { get; set; }
         private int IdEndereco { get; set;}
         private virtual Endereco endereco { get; set;}
         
---
+++ Models/Caracteristica.cs
@@ -0,0 +1,17 @@
+namespace sistemaDeAdocaoParaAnimais.Models
+{
+    public class Caracteristica
+    {
+        private int Energia { get; set; }
+        private int Humor { get; set; }
+        private int Apego { get; set; }
+        private int Latido { get; set; }
+        private int Inteligencia { get; set; }
+        private int Brincadeira { get; set; }
+        private int Adestramento { get; set; }
+        private bool Vacinado { get; set; }
+        private bool Castrado { get; set; }
+        private bool Vermifugado { get; set; }
+
+    }
+}
