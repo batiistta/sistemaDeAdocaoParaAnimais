750fbbb57d1172fb0f4b5cbb23332c3ab4a6af74
Rafael Rascado <rafaelrascado98@gmail.com>
2022-12-04 22:02:42 -0300

    style: iniciando o layout responsivo

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -28,6 +28,7 @@ namespace sistemaDeAdocaoParaAnimais.Models
 
         [Required(ErrorMessage = "Campo Obrigatório.")]
         [Display(Name = "Conte um pouco da sua historia com o pet")]
+        [StringLength(623, ErrorMessage = "A historia do pet nao pode conter mais de 115 caracrteres. ")]
         public string Descricao { get; set; }
 
         [Required(ErrorMessage = "Campo Obrigatório.")]
@@ -45,7 +46,7 @@ namespace sistemaDeAdocaoParaAnimais.Models
         [Required]
         [Display(Name = "Estado")]
         public string Estado { get; set; }
-        
+
         [Required]
         [Display(Name = "Cidade")]
         public string Cidade { get; set; }
