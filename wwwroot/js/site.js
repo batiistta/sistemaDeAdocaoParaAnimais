4d91b0f282239d71981d9cc84a8e8c7c24198f45
paulocesarDS <paulocry67@gmail.com>
2022-12-08 21:43:17 -0300

    Feat: Popup in ExibirPet

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -147,3 +147,12 @@ inputPath.addEventListener("change", function () {
     }
 });
 
+
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
